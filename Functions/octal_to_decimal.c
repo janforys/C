196833714ad218ce981e1e11bacbd4f13c29298d
janforys <janforys@wp.pl>
2022-09-14 22:51:42 +0200

    DEC->OCT done

--- Functions/octal_to_decimal.c
+++ Functions/octal_to_decimal.c
@@ -41,36 +41,20 @@ int octal2decimal(int octalValue) {
 
 int decimal2octal(int decimalValue) {
     int multiplier = 1, sum = 0, octal = 0;
-    int value = decimalValue;
     for (int k = decimalValue; k >= 1; k /= 10) {
         multiplier *= 10;
     }
     int m = multiplier;
-    decimalValue *= 10;
     while (decimalValue >= 1) {
-        int temp = value % 8;
-        //sum += value % 8 * multiplier;
-            if (value == 1) {
-                 temp = value;   
-            }
-        printf("\n%d", temp);
-        sum += temp * multiplier;
-        printf("\n");
-        printf("%d", sum);
-        value /= 8; // 8 because it is octal
-        printf("\n%d", value);
-        decimalValue /= 10;
+        sum += decimalValue % 8 * multiplier;
+        decimalValue /= 8; // 8 because it's octal
         multiplier /= 10; // needed to concatenate
     }
-    printf("\n");
-    //sum /= 10;
-    
-    /* Reversing octal result */
-    /*while (sum >= 1) {
+    /* Reversing sum to get octal result */
+    while (sum >= 1) {
         octal += sum % 10 * m; 
         sum /= 10;
         m /= 10;
     }
-    octal /= 10;*/
-    return 666;
+    return octal;
 }

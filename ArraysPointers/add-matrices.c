e45e641978bb81cea73d637186b1897db2e1da6a
janforys <janforys@wp.pl>
2022-09-28 21:15:04 +0200

    add-matrices.c -update

--- ArraysPointers/add-matrices.c
+++ ArraysPointers/add-matrices.c
@@ -20,17 +20,17 @@ int array2[x2][y2];
 /* array1 */
 printf("____Array 1____\n");
 for (int i = 0; i < x1; i++) {
-for (int k = 0; k < y1; k++) {
-printf("Enter element for array1[%d][%d]: ", i, k);
-scanf("%d", &array1[i][k]);
+ for (int k = 0; k < y1; k++) {
+	printf("Enter element for array1[%d][%d]: ", i, k);
+	scanf("%d", &array1[i][k]);
 }
 }
 /* array2 */
 printf("\n____Array 2____\n");
 for (int m = 0; m < x2; m++) {
-for (int n = 0; n < y2; n++) {
-printf("Enter element for array2[%d][%d]: ", m, n);
-scanf("%d", &array2[m][n]);
+ for (int n = 0; n < y2; n++) {
+	printf("Enter element for array2[%d][%d]: ", m, n);
+	scanf("%d", &array2[m][n]);
 }
 }
 
@@ -38,15 +38,15 @@ scanf("%d", &array2[m][n]);
 /* array1 */
 printf("\nElements of an Array 1:\n");
 for (int i = 0; i < x1; i++) {
-for (int k = 0; k < y1; k++) {
-printf("%d ", array1[i][k]);
+ for (int k = 0; k < y1; k++) {
+	printf("%d ", array1[i][k]);
 }
 }
 /* array2 */
 printf("\nElements of an Array 2:\n");
 for (int m = 0; m < x2; m++) {
-for (int n = 0; n < y2; n++) {
-printf("%d ", array2[m][n]);
+ for (int n = 0; n < y2; n++) {
+	printf("%d ", array2[m][n]);
 }
 }
 
@@ -55,8 +55,8 @@ printf("\n_________________________\n");
 /* Add matrices */
 printf("Sum of matrices: ");
 for (int a = 0; a < x1; a++) {
-for (int b = 0; b < y1; b++) {
-printf("%d ", array1[a][b] + array2[a][b]);
+ for (int b = 0; b < y1; b++) {
+	printf("%d ", array1[a][b] + array2[a][b]);
 }
 }
 

a9596bab5faf26035d81211076c675651e069e27
janforys <janforys@wp.pl>
2022-10-04 17:19:23 +0200

    Update

--- ArraysPointers/average.c
+++ ArraysPointers/average.c
@@ -12,19 +12,21 @@ int main() {
  scanf("%d", &index);
  float array[index];
 
- /* Save into */
+ /* Save into an array */
  printf("Type elements to calculate: ");
  for (int i = 0; i < index; ++i) {
   scanf("%f", &array[i]);
  }
 
- /* Print from */
+ /* Print from an array */
  for (int i = 0; i < index; ++i) {
   float sum;
   average = (sum += array[i]) / index;
  }
 
- printf("Average = %f\n\n", average);
+ printf("Average = %f", average);
+
+ printf("\n\nPRESS CTRL+C TO EXIT\n");
  pause();
  return 0;
 }

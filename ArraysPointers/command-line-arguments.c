778c7c0a63166cd1d75354a4fa4ee8eb0135d2e5
janforys <janforys@wp.pl>
2022-10-10 21:15:51 +0200

    command line arguments

--- ArraysPointers/command-line-arguments.c
+++ ArraysPointers/command-line-arguments.c
@@ -1,1 +1,26 @@
-/**/
+/* Command Line Arguments */
+/* example from: "The C Programming language" book */
+
+
+#include <stdio.h>
+
+/* Print arguments (v1) */
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 1; i < argc; i++)
+		// operate array
+		printf("%s%s", argv[i], (i < argc - 1) ? " " : "");
+	printf("\n");
+	return 0;
+}
+
+/* Print arguments (v2) */
+int main2(int argc, char *argv[])
+{
+	while (--argc > 0)
+		// operate pointer value
+		printf("%s%s", *++argv, (argc > 1) ?  " " : "");
+	printf("\n");
+	return 0;
+}

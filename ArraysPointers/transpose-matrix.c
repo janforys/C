f52cd7160c33046e7287695d546c2a90ee621d15
janforys <janforys@wp.pl>
2022-10-16 21:31:28 +0200

    transpose-matrix

---
+++ ArraysPointers/transpose-matrix.c
@@ -0,0 +1,25 @@
+/* C Program to Find Transpose of a Matrix */
+
+
+#include <stdio.h>
+#include <unistd.h>
+
+void transpose(); 
+
+int main() {
+
+int x1, y1, x2, y2;
+
+printf("Enter rows, columns: \n");
+scanf("%d %d", &x1, &y1);
+
+printf("Enter rows, columns: \n");
+scanf("%d %d", &x1, &y2);
+
+//void transpose(int, int, int, int);
+
+printf("\nPRESS CTRL+C TO EXIT\n");
+pause();
+
+return 0;
+}

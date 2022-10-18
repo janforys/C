8466e0cc266af6e37358d2a2abbf6baa5d0b7136
janforys <janforys@wp.pl>
2022-10-18 21:05:22 +0200

    transpose matrix

--- ArraysPointers/transpose-matrix.c
+++ ArraysPointers/transpose-matrix.c
@@ -8,18 +8,47 @@ void transpose();
 
 int main() {
 
-int x1, y1, x2, y2;
+const int x1, y1, x2, y2;
 
 printf("Enter rows, columns: \n");
 scanf("%d %d", &x1, &y1);
 
 printf("Enter rows, columns: \n");
-scanf("%d %d", &x1, &y2);
+scanf("%d %d", &x2, &y2);
 
-//void transpose(int, int, int, int);
+transpose(x1, y1, x2, y2);
 
 printf("\nPRESS CTRL+C TO EXIT\n");
 pause();
 
 return 0;
+}
+
+void transpose(int x1, int y1, int x2, int y2) {
+
+int m1[x1][y1];
+int m2[x2][y2];
+
+/* Fill matrices */
+/* First matrix */
+for (int i = 0; i < x1; i++)
+	for (int j = 0; j < y1; j++)
+		scanf("%d", &m1[i][j]);
+	
+/* Second matrix */
+for (int i = 0; i < x2; i++)
+	for (int j = 0; j < y2; j++)
+		scanf("%d", &m2[i][j]);
+
+/* Print out matrices */
+/* First matrix */
+for (int i = 0; i < x1; i++)
+	for (int j = 0; j < y1; j++)
+		printf("%d", m1[i][j]);
+	
+/* Second matrix */
+for (int i = 0; i < x2; i++)
+	for (int j = 0; j < y2; j++)
+		printf("%d", m2[i][j]);
+
 }

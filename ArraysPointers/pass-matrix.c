dec17bd66d1f1aedcfef0a87bcc61f82f8b232bb
janforys <janforys@wp.pl>
2022-11-10 20:50:08 +0100

    Add new file

---
+++ ArraysPointers/pass-matrix.c
@@ -0,0 +1,15 @@
+/* C Program to Multiply two Matrices by Passing Matrix to a Function */
+
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+
+
+
+printf("\n\nPRESS CTRL+C TO EXIT\n");
+pause();
+
+return 0;
+}
